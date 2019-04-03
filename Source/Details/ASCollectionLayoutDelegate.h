2beb64ab1dd5299db732a441f203d821871899dc
Victor Stewart <vdog5@mac.com>
2019-04-03 15:32:57 -0400

    Update ASCollectionLayoutDelegate.h

--- Source/Details/ASCollectionLayoutDelegate.h
+++ Source/Details/ASCollectionLayoutDelegate.h
@@ -54,6 +54,8 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (ASCollectionLayoutState *)calculateLayoutWithContext:(ASCollectionLayoutContext *)context;
 
+- (ASCollectionLayoutState *)sneaky_calculateLayoutWithContext:(ASCollectionLayoutContext *)context;
+
 @end
 
 NS_ASSUME_NONNULL_END
